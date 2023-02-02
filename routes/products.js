72d70e123157d05cc5162aa1c32d6856d4e401cb
dauqu <sujeetsingh734321@gmail.com>
2023-02-02 16:35:18 +0530

    nxt change

--- routes/products.js
+++ routes/products.js
@@ -115,28 +115,37 @@ router.get("/sku/:sku", async (req, res) => {
 });
 
 // code to update a product by id
-router.put("/:id", async (req, res) => {
+router.put("/:productId", async (req, res) => {
   try {
-    const response = await productsCollection.doc(req.params.id).update({
-      title: req.body.title,
-      description: req.body.description,
-      price: req.body.price,
-      saleprice: req.body.saleprice,
-      image: req.body.image,
-      vendor: req.body.vendor,
-      status: req.body.status,
-      category: req.body.category,
-      type: req.body.type,
-      featured: req.body.featured,
-      language: req.body.language,
-      updatedAt: new Date().toISOString(),
-    });
-    res.json({
-      message: "Product updated successfully",
-      response,
-    });
+    const response = await productsCollection.doc(req.params.productId);
+
+    const updatedata = await response
+      .set(
+        {
+          title: req.body.title,
+          description: req.body.description,
+          price: req.body.price,
+          saleprice: req.body.saleprice,
+          image: req.body.image,
+          vendor: req.body.vendor,
+          status: req.body.status,
+          category: req.body.category,
+          type: req.body.type,
+          featured: req.body.featured,
+          language: req.body.language,
+          updatedAt: new Date().toISOString(),
+        },
+        {
+          merge: true,
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      });
+    // res.send(response);
+    res.json({ response: response });
   } catch (error) {
-    res.send(error);
+    res.json({ message: error.message });
   }
 });
 
